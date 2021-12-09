e3794970cdd14f046c42e31773d9fd611110e235
dragana <stefanoviic.dragana@gmail.com>
2021-12-09 21:01:43 +0100

    Dodati kontroleri za svaki model.

---
+++ app/Http/Controllers/TVShowController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TVShow;
+use Illuminate\Http\Request;
+
+class TVShowController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TVShow  $tVShow
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TVShow $tVShow)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TVShow  $tVShow
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TVShow $tVShow)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\TVShow  $tVShow
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TVShow $tVShow)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TVShow  $tVShow
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TVShow $tVShow)
+    {
+        //
+    }
+}
